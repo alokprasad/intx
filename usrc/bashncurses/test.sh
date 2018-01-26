05fb3919686e0cd22bb8131f869de447054ed0e5
alokprasad <alok.prasad7@gmail.com>
2018-01-26 10:26:21 +0530

    initial

---
+++ usrc/bashncurses/test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#import library, please check path
+#source /usr/lib/simple_curses.sh
+source simple_curses.sh
+
+#Then, you must create a "main" function:
+#main (){
+    #your code here, you can add some windows, text...
+    window "title" "color"
+    append "Text..."
+    endwin
+#}
+
+#then, you can execute loop:
+#main_loop 1
