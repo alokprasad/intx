787e5c5d73cc5570deef8dd38f822f0479c7f936
alokprasad <alok.prasad7@gmail.com>
2018-01-26 17:00:38 +0530

    final

--- usrc/gcc_inst_func/gcc_profiling_a2l.c
+++ usrc/gcc_inst_func/gcc_profiling_a2l.c
@@ -18,18 +18,19 @@ void __cyg_profile_func_enter (void *this_fn, void *call_site)
 	char cmd[80];
 	//printf("Function Entry : %p %p \n", this_fn, call_site);
 	//sprintf(cmd,"addr2line -e a.out %p",this_fn);
+	printf("\n--->Enter\n");
 	sprintf(cmd,"nm a.out | grep %llx",this_fn);
 	system(cmd);
-	printf("--->Enter\n");
 }
 
 void __cyg_profile_func_exit (void *this_fn, void *call_site)
 {
 	//printf("Function Exit : %p %p \n", this_fn, call_site);
 	char cmd[80];
+	
+	printf("\n--->Exit\n");
 	sprintf(cmd,"nm a.out | grep %llx",this_fn);
 	system(cmd);
-	printf("--->Exit\n");
 }
 
 int main()
