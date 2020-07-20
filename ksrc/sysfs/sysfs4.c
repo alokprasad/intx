fb0650f28131c383797c83cb47e3a4775256f459
alokprasad <alok.prasad7@gmail.com>
2020-07-20 10:20:02 +0530

    Update sysfs4.c

--- ksrc/sysfs/sysfs4.c
+++ ksrc/sysfs/sysfs4.c
@@ -4,11 +4,13 @@
 
 int temp = 10;
 
+// show function should be informa __ATTR_RO/RW(name) --->   .show = _name##_show
 static ssize_t test_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
 	return sprintf(buf, "%d\n", temp);
 }
 
+// show function should be informa __ATTR_RO/RW(name) --->   .store = _name##_store
 static ssize_t test_store(struct kobject *kobj, struct kobj_attribute *attr, char *buf , size_t len)
 {
     sscanf(buf,"%d",&temp);
@@ -16,13 +18,15 @@ static ssize_t test_store(struct kobject *kobj, struct kobj_attribute *attr, cha
 }
 
 
-static const struct kobj_attribute test_attribute = __ATTR_RO(test);
+static const struct kobj_attribute test_attribute = __ATTR_RW(test);
+
  
 static int __init example_init(void)
 {
 
-	sysfs_create_file(kernel_kobj, &test_attribute.attr);
-
+	//Create entry in /sys/kernel/test
+		sysfs_create_file(kernel_kobj, &test_attribute.attr);
+	
 	return 0;
 }
 
